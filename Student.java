7433770510d5f3ad1246bbee85a2c4291d35a000
namrathapookkattiri-28 <namrathapookkattiri28@gmail.com>
2025-12-29 06:11:31 -0800

    Add Student class with id, name, and grade fields

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private int id;
+    private String name;
+    private String grade;
+
+    public Student(int id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+}
